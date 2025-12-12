98567cb0500799585a403463a52635cfdd2b951a
arfdev <arsegy12@gmail.com>
2025-12-12 08:11:09 +0700

    Add Firebase configuration and initialization
    Added Firebase configuration and initialization.

--- frontend/public/firebase-config.js
+++ frontend/public/firebase-config.js
@@ -1,1 +1,14 @@
+// firebase-config.js
 
+const firebaseConfig = {
+  apiKey: "AIzaSyB...", // Ganti dengan apiKey Anda
+  authDomain: "Rawuh.id.firebaseapp.com",
+  projectId: "Rawuh.id-120808", // Ganti dengan Project ID Anda
+  storageBucket: "Rawuh.id.appspot.com",
+  messagingSenderId: "1234567890",
+  appId: "1:1234567890:web:abcdef"
+};
+
+// Inisialisasi Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
